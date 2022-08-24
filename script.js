85ed2d80f7b059777f1504dc362cd775a4c8e598
JonLedo <jonledo.code@gmail.com>
2022-08-24 01:43:43 -0400

    all basic functions for app. bugs to be fixed, and styling needs to be fixed

--- script.js
+++ script.js
@@ -95,6 +95,41 @@ const questions = [
   }
 ]
 
+const answers = [
+  {
+    combination: ["New York", "Pizza", "Condo" ],
+    text: "Blue Cheese",
+    image: "https://images.unsplash.com/photo-1626957341926-98752fc2ba90?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+    alt: "photo of blue cheese",
+    credit: "Towfiqu barbhuiya"
+  },
+  {
+    combination: ["Toronto", "Salad", "House" ],
+    text: "Cheddar",
+    image: "https://images.unsplash.com/photo-1618164436241-4473940d1f5c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+    alt: "photo of cheddar cheese",
+    credit: "Önder Örtel"
+  },
+  {
+    combination: ["Paris", "BBQ", "House" ],
+    text: "Brie",
+    image: "https://images.unsplash.com/photo-1588350492670-e2f4a16a4fc6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+    alt: "photo of brie cheese next to slices of bread",
+    credit: "iam_os"
+  },
+  {
+    combination: ["London", "Pizza", "Condo" ],
+    text: "Mozarella Balls",
+    image: "https://images.unsplash.com/photo-1600455575515-715819f9ac39?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=688&q=80",
+    alt: "photo of mozarella balls in a bowl",
+    credit: "serjan midili"
+  }
+
+]
+
+const unansweredQuestions = []
+const chosenAnswers = []
+
 function populateQuestions () {
   questions.forEach(question => {
     const titleBlock = document.createElement('div')
@@ -110,10 +145,14 @@ function populateQuestions () {
     answersBlock.id = question.id + "-questions"
     answersBlock.classList.add('answer-options')
 
+    unansweredQuestions.push(question.id)
+
     question.answers.forEach(answer => {
       const answerBlock = document.createElement('div')
       answerBlock.classList.add('answer-block')
-      answerBlock.addEventListener('click', handleClick)
+      answerBlock.addEventListener('click', () => handleClick(question.id, answer.text))
+
+
       const answerImage = document.createElement('img')
       answerImage.setAttribute('src', answer.image)
       answerImage.setAttribute('alt', answer.alt)
@@ -141,6 +180,67 @@ function populateQuestions () {
 
 populateQuestions()
 
-function handleClick() {
-  console.log('clicked')
+function handleClick(questionId, chosenAnswer) {
+  if (unansweredQuestions.includes(questionId)){
+    chosenAnswers.push(chosenAnswer)
+  }
+  const itemToRemove = unansweredQuestions.indexOf(questionId)
+
+  if (itemToRemove > -1) {
+    unansweredQuestions.splice(itemToRemove, 1)
+  }
+
+  disableQuestionBlock(questionId, chosenAnswer)
+  const lowestquestionId = Math.min(...unansweredQuestions)
+  location.href = `#${lowestquestionId}`
+
+
+  if (!unansweredQuestions.length) {
+    // scroll to answer div
+    location.href = '#answer'
+    showAnswer()
+  }
+}
+
+function showAnswer() {
+  let result
+  answers.forEach(answer => {
+    if (
+      chosenAnswers.includes(answer.combination[0]) + 
+      chosenAnswers.includes(answer.combination[1]) + 
+      chosenAnswers.includes(answer.combination[2])
+    ) {
+      result = answer
+    } else {
+      // first anwser object is default
+      result = answers[0]
+    }
+  })
+
+  const answerBlock = document.createElement('div')
+  answerBlock.classList.add('result-block')
+  const answerTitle = document.createElement('h3')
+  answerTitle.textContent = result.text
+  const answerImage = document.createElement('img')
+  answerImage.setAttribute('src', result.image)
+  answerImage.setAttribute('alt', result.alt)
+
+  answerBlock.append(answerTitle, answerImage)
+  answerDisplay.append(answerBlock)
+
+
+  const allAnswerBlocks = document.querySelectorAll('.answer-block')
+  Array.from(allAnswerBlocks).forEach(answerBlock => {
+    answerBlock.replaceWith(answerBlock)
+  })
+}
+
+function disableQuestionBlock (questionId, chosenAnswer) {
+  const currentQuestionBlock = document.getElementById(questionId + "-questions")
+
+  Array.from(currentQuestionBlock.children).forEach(block => {
+    if (block.children.item(1).innerText !== chosenAnswer) {
+      block.style.opacity = "50%"
+    }
+  })
 }
