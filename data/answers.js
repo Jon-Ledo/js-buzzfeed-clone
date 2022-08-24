31e7674dcd177fde3adb8e06d6fc1db526f0378d
JonLedo <jonledo.code@gmail.com>
2022-08-24 16:07:11 -0400

    type module included, sepearted data into seperate folder files

---
+++ data/answers.js
@@ -0,0 +1,31 @@
+export const answers = [
+  {
+    combination: ["New York", "Pizza", "Condo" ],
+    text: "Blue Cheese",
+    image: "https://images.unsplash.com/photo-1626957341926-98752fc2ba90?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+    alt: "photo of blue cheese",
+    credit: "Towfiqu barbhuiya"
+  },
+  {
+    combination: ["Toronto", "Salad", "House" ],
+    text: "Cheddar",
+    image: "https://images.unsplash.com/photo-1618164436241-4473940d1f5c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+    alt: "photo of cheddar cheese",
+    credit: "Önder Örtel"
+  },
+  {
+    combination: ["Paris", "BBQ", "House" ],
+    text: "Brie",
+    image: "https://images.unsplash.com/photo-1588350492670-e2f4a16a4fc6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+    alt: "photo of brie cheese next to slices of bread",
+    credit: "iam_os"
+  },
+  {
+    combination: ["London", "Pizza", "Condo" ],
+    text: "Mozarella Balls",
+    image: "https://images.unsplash.com/photo-1600455575515-715819f9ac39?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=688&q=80",
+    alt: "photo of mozarella balls in a bowl",
+    credit: "serjan midili"
+  }
+
+]
